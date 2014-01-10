7195d790caddc11ff6fabb73828b630e67489a64
AknEp <akn@s-satoshi.net>
2014-01-11 00:11:18 +0900

    Initial Commit

---
+++ BugReproductionBlocksKit/AKNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKNAppDelegate.h
+//  BugReproductionBlocksKit
+//
+//  Created by Satoshi SUZUKI on 2014/01/11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BugReproductionBlocksKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BugReproductionBlocksKit
+//
+//  Created by Satoshi SUZUKI on 2014/01/11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AKNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AKNAppDelegate class]));
+    }
+}
